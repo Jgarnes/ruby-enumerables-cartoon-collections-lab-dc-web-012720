8b231215bebbc5b8c2f2c4cc4625426c696ce4c5
Jeremy Garnes <j06garnes@gmail.com>
2020-01-14 03:46:43 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -21,10 +21,9 @@ def long_planeteer_calls(array)# code an argument here
  if n.length > 4
     return true
  end
- 
+ end
  return false
-end
- end 
+end 
 #def find_the_cheese(array)# code an argument here
   # the array below is here to help
  # cheese_types = ["cheddar", "gouda", "camembert"]
