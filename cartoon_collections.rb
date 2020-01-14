6463940cc78402c6fc4ffe792ea04c85e81c7470
Jeremy Garnes <j06garnes@gmail.com>
2020-01-14 01:01:00 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,26 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)# code an argument here
+ i = 1 
+ while i < array.length do 
+  array.map do |n|
+    puts "#{i} #{n}" 
+  i += 1
+  end 
+ end 
+end 
+
+def summon_captain_planet(array)# code an argument here
+  new_array = []
+  array.collect do |w|
+    new_array << "#{w.capitalize}!"
+  end 
+  new_array
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def long_planeteer_calls(array)# code an argument here
+  
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
-end
-
-def find_the_cheese# code an argument here
+def find_the_cheese(array)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
