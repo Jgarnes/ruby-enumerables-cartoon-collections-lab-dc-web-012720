304861c6c7c34bbcfff9094f92b3cc11b36f26c9
Jeremy Garnes <j06garnes@gmail.com>
2020-01-14 01:52:20 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,7 +17,8 @@ def summon_captain_planet(array)# code an argument here
 end
 
 def long_planeteer_calls(array)# code an argument here
-  
+  array.find do |w|
+    
 end
 
 def find_the_cheese(array)# code an argument here
