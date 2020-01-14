06e84b1d6e8b670752a37a478a8254d39c3d47fa
Jeremy Garnes <j06garnes@gmail.com>
2020-01-14 04:43:53 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -27,13 +27,11 @@ end
 
 def find_the_cheese(array)# code an argument here
   # the array below is here to help
-  i = 0 
+   
   cheese_types = ["cheddar", "gouda", "camembert"]
-  while i < cheese_types.length do 
-  array.each do |word|
-    word.include?(cheese_types[i])
+   
+  cheese_types.each do |word|
+    word.include?(array)
     
-end
-i += 1 
 end 
 end 
