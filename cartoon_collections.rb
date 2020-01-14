dd9c5809ae0acde51e07afefa4ee47aaad781da6
Jeremy Garnes <j06garnes@gmail.com>
2020-01-14 05:15:25 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -30,8 +30,8 @@ def find_the_cheese(array)# code an argument here
    
   cheese_types = ["cheddar", "gouda", "camembert"]
    
-  cheese_types.each do |word|
-    if word.include?(array)
+  array.each do |word|
+    if word.include?()
       return word 
     end
 end
