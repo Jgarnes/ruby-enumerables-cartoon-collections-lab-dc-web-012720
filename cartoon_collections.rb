3bbfd3303bd08a990f6d94f2c4f84c9a5cee36b1
Jeremy Garnes <j06garnes@gmail.com>
2020-01-14 02:29:26 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,16 +17,17 @@ def summon_captain_planet(array)# code an argument here
 end
 
 def long_planeteer_calls(array)# code an argument here
-  
   word = array.each do |i|
     if word.find <= 4
       put true 
     else 
       false
-       
+    end   
+    
+    
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
