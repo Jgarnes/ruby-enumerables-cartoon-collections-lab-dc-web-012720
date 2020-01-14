5d1ba5431e0e470892b2a4a179bd74e15e3d8461
Jeremy Garnes <j06garnes@gmail.com>
2020-01-14 02:42:04 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,7 +17,7 @@ def summon_captain_planet(array)# code an argument here
 end
 
 def long_planeteer_calls(array)# code an argument here
- if array.length > 4 
+ if array.length >= 5 
     true 
   else array.length <= 4
     false 
